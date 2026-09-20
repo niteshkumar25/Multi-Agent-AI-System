6b19250287a5ea9bd31123bce10cf8e7f38b3caf
niteshkumar259 <niteshkumar2599@gmail.com>
2026-09-21 00:58:13 +0200

    added memory for AI agent

--- backend/services/agent/controllers/agent.controller.js
+++ backend/services/agent/controllers/agent.controller.js
@@ -5,8 +5,6 @@ export const agent = async (req,res)=>{
     try {
         const {prompt, conversationId} = req.body
 
-        //ADD Message in radis 
-        await addMessage(conversationId,"user", prompt) 
       
         await axios.post(`${process.env.CHATSERVICE_URL}/save-message`,{
             conversationId,
@@ -20,6 +18,8 @@ export const agent = async (req,res)=>{
         })
 
         const response = result.aiResponse
+
+         await addMessage(conversationId,"user", prompt) 
 
          await addMessage(conversationId,"assistant", response)
 
